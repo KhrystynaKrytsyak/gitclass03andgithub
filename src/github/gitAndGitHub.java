cf51cb81f738feee93af81e029a8b1d89fdb47ba
KhrystynaKrytsyak <krytsyak@gmail.com>
2022-05-31 19:54:31 -0400

    2nd commit

--- src/github/gitAndGitHub.java
+++ src/github/gitAndGitHub.java
@@ -1,10 +1,13 @@
 package github;
 
 public class gitAndGitHub {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.print("today we learn github");
+			System.out.print("today is a very good day");
+			
+		}
 	}
 
-}
+
